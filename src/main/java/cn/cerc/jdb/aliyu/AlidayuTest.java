928b1cf572c4b7eb6bf2babc7456867d631d89d3
张弓 <sz9214e@qq.com>
2017-01-16 20:40:17 +0800

    增加阿里大于测试用例

---
+++ src/main/java/cn/cerc/jdb/aliyu/AlidayuTest.java
@@ -0,0 +1,36 @@
+package cn.cerc.jdb.aliyu;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import cn.cerc.jdb.core.IConfig;
+
+public class AlidayuTest {
+
+	@Test
+	@Ignore
+	public void test() {
+		Alidayu sms = new Alidayu(new IConfig() {
+
+			@Override
+			public String getProperty(String key, String def) {
+				if (Alidayu.AppKey.equals(key))
+					return "appkey";
+				if (Alidayu.AppSecret.equals(key))
+					return "appsecret";
+				if (Alidayu.SingName.equals(key))
+					return "我的应用";
+				return null;
+			}
+
+		});
+		sms.setMobileNo("13912345678");
+		sms.setTemplateNo("SMS_1190006");
+		boolean ok = sms.send("000000", this);
+		System.out.println(sms.getMessage());
+		assertTrue(ok);
+	}
+
+}
