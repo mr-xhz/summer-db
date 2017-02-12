91fccd63fd02e409d4523018ff82a9072bd8fcfb
张弓 <sz9214e@qq.com>
2017-02-13 07:06:15 +0800

    增加对ms sql 数据库的支持

---
+++ src/main/java/cn/cerc/jdb/mssql/MssQuery.java
@@ -0,0 +1,32 @@
+package cn.cerc.jdb.mssql;
+
+import cn.cerc.jdb.core.DataQuery;
+import cn.cerc.jdb.core.IDataOperator;
+import cn.cerc.jdb.core.IHandle;
+
+public class MssQuery extends DataQuery{
+	private static final long serialVersionUID = 889285738942368226L;
+
+	public MssQuery(IHandle handle) {
+		super(handle);
+	}
+
+	@Override
+	public DataQuery open() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public IDataOperator getOperator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
