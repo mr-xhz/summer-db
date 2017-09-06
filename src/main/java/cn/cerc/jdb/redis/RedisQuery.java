6cf6b931740cd6f46f35f26d3e756f7856703569
CrazyPag <1258020400@qq.com>
2017-09-06 09:27:40 +0800

    添加redis缓存机制，包括redis五种注值方式以及相关查询方法
    添加redis缓存机制，包括redis五种注值方式以及相关查询方法

--- src/main/java/cn/cerc/jdb/redis/RedisQuery.java
+++ src/main/java/cn/cerc/jdb/redis/RedisQuery.java
@@ -6,29 +6,29 @@ import cn.cerc.jdb.core.IHandle;
 
 public class RedisQuery extends DataQuery {
 
-    private static final long serialVersionUID = 5655647321461069483L;
+	private static final long serialVersionUID = 5655647321461069483L;
+	public static RedisSession sess;
+	public RedisQuery(IHandle handle) {
+		super(handle);
+		sess = (RedisSession) handle.getProperty(RedisSession.sessionId);
+	}
 
-    public RedisQuery(IHandle handle) {
-        super(handle);
-        // TODO Auto-generated constructor stub
-    }
+	@Override
+	public DataQuery open() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    @Override
-    public DataQuery open() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	@Override
+	public void save() {
+		// TODO Auto-generated method stub
+		
+	}
 
-    @Override
-    public void save() {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public IDataOperator getOperator() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	@Override
+	public IDataOperator getOperator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
