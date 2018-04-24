c447034461cd26e3f56ba2af63b3d4bfd7644c31
ZhangGong <sz9214e@qq.com>
2018-04-24 19:51:49 +0800

    增加微服务调用工具

---
+++ src/main/java/cn/cerc/jdb/core/MicroService.java
@@ -0,0 +1,101 @@
+package cn.cerc.jdb.core;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class MicroService extends Curl {
+    /** 服务主机的地址 */
+    private String host = "http://127.0.0.1";
+    /** 服务主机的port */
+    private int port = 80;
+    /** 服务前缀 */
+    private String path;
+    /** 返回结果 */
+    private boolean result;
+    /** 返回消息 */
+    private String message;
+    /** 返回内容转为gson */
+    private JsonObject data;
+
+    /** 执行指定的服务 */
+    public boolean get(String serviceCode) {
+        String url = getServiceUrl(serviceCode);
+        String text = this.doGet(url);
+        this.data = new JsonParser().parse(text).getAsJsonObject();
+        if (data.has("result"))
+            this.result = data.get("result").getAsBoolean();
+        if (data.has("message"))
+            this.message = data.get("message").getAsString();
+        return this.result;
+    }
+
+    /** 执行指定的服务 */
+    public boolean post(String serviceCode) {
+        String url = getServiceUrl(serviceCode);
+        String text = this.doPost(url);
+        this.data = new JsonParser().parse(text).getAsJsonObject();
+        if (data.has("result"))
+            this.result = data.get("result").getAsBoolean();
+        if (data.has("message"))
+            this.message = data.get("message").getAsString();
+        return this.result;
+    }
+
+    private String getServiceUrl(String serviceCode) {
+        StringBuffer sb = new StringBuffer();
+        sb.append(this.host);
+        if (port != 80)
+            sb.append(":" + this.port);
+        if (this.path != null)
+            sb.append(this.path);
+        sb.append("/" + serviceCode);
+        return sb.toString();
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public boolean isResult() {
+        return result;
+    }
+
+    public JsonObject getData() {
+        return data;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public static void main(String[] args) {
+        MicroService service = new MicroService();
+        service.addParameter("appKey", "asdfsdf");
+        
+        System.out.println(service.post("server.getIP"));
+        System.out.println(service.isResult());
+        System.out.println(service.getMessage());
+        System.out.println(service.getResponseContent());
+        System.out.println(service.getData().toString());
+    }
+}
